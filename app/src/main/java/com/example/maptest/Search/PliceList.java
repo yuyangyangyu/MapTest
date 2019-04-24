3621f87bf0e45b24dddc023c9cbb690723d6783d
yuyangyangyu <1813233580@qq.com>
2019-04-24 11:09:22 +0800

    新增距离和推荐指数

--- app/src/main/java/com/example/maptest/Search/PliceList.java
+++ app/src/main/java/com/example/maptest/Search/PliceList.java
@@ -3,6 +3,8 @@ package com.example.maptest.Search;
 public class PliceList {
     private String Name;
     private String Distance;
+    private String mark_1;
+    private String mark_2;
     public PliceList(String Name,String Distance){
         this.Name=Name;
         this.Distance=Distance;
